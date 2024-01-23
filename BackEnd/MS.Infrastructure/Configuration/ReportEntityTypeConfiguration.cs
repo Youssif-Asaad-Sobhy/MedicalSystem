80d9b70bbe1a007a1550536fbbd20aa691137eca
Alaa Mobarak <3laa.m0o0barak@gmail.com>
2024-01-23 13:02:35 +0200

    drgf

--- BackEnd/MS.Infrastructure/Configuration/ReportEntityTypeConfiguration.cs
+++ BackEnd/MS.Infrastructure/Configuration/ReportEntityTypeConfiguration.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MS.Data.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace MS.Infrastructure.Configuration
+namespace MS.Data.Configuration
 {
-    internal class ReportEntityTypeConfigration
+    public class ReportEntityTypeConfiguration : IEntityTypeConfiguration<Report>
     {
+        public void Configure(EntityTypeBuilder<Report> builder)
+        {
+
+
+        }
     }
 }
