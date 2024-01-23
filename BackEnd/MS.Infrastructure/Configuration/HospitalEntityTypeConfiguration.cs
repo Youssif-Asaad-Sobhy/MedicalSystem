e537f2b7da1380a6d6d2cf53e9c3d2402d9280d4
Zeyad Abdelnaby <zeyadabdelnaby15@gmail.com>
2024-01-23 15:24:16 +0200

    added DI Validator

--- BackEnd/MS.Infrastructure/Configuration/HospitalEntityTypeConfiguration.cs
+++ BackEnd/MS.Infrastructure/Configuration/HospitalEntityTypeConfiguration.cs
@@ -6,7 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using MS.Data.Entities;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-
+using FluentValidation;
+using MS.Infrastructure.Validation;
 namespace MS.Infrastructure.Configuration
 {
     public class HospitalEntityTypeConfiguration : IEntityTypeConfiguration<Hospital>
@@ -14,7 +15,6 @@ namespace MS.Infrastructure.Configuration
         public void Configure(EntityTypeBuilder<Hospital> builder)
         {
             
-
         }
     }
 }
--- BackEnd/MS.Infrastructure/Infrastructure.cs
+++ BackEnd/MS.Infrastructure/Infrastructure.cs
@@ -1,7 +1,15 @@
-﻿namespace MS.Infrastructure
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using MS.Data.Entities;
+using MS.Infrastructure.Validation;
+
+namespace MS.Infrastructure
 {
     public class Infrastructure
     {
-
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IValidator<Medicine>, MedicineValidator>();
+        }
     }
 }
