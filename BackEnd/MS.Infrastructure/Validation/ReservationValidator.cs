3c31d96b4721af63e5466a6a7bf7b87a53284721
Youssif-Asaad-Sobhy <youssifasaadfci@gmail.com>
2024-01-23 13:36:31 +0200

    update

---
+++ BackEnd/MS.Infrastructure/Validation/ReservationValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using MS.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS.Infrastructure.Validation
+{
+    internal class ReservationValidator : AbstractValidator<Reservation>
+    {
+        public ReservationValidator() 
+        {
+            RuleFor(x=>x.ID)
+                .NotNull()
+                .NotEmpty
+        }
+    }
+}
---
+++ BackEnd/MS.Infrastructure/Validation/TestLabValidator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using MS.Data.Entities;
+
+namespace MS.Infrastructure.Validation
+{
+    public class TestLabValidator : AbstractValidator<TestLab>
+    {
+        public TestLabValidator()
+        {
+            RuleFor(x => x.ID)
+                .NotEmpty()
+                .NotNull();
+            RuleFor(x=>x.TestID)
+                .NotEmpty()
+                .NotNull();
+            RuleFor(x=>x.LabID)
+                .NotEmpty()
+                .NotNull();
+            RuleFor(x=>x.Price)
+                .NotEmpty()
+                .NotNull()
+                .LessThan(100000)
+                .GreaterThanOrEqualTo(0);
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                .MaximumLength(10000)
+                .NotNull();
+        }
+    }
+}
