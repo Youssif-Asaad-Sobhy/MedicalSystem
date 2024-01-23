cf09909ded36ae135cf2b6d6d76f3e243670dc8a
Youssif-Asaad-Sobhy <youssifasaadfci@gmail.com>
2024-01-23 15:34:25 +0200

    added validation for all classes

--- BackEnd/MS.Infrastructure/Infrastructure.cs
+++ BackEnd/MS.Infrastructure/Infrastructure.cs
@@ -10,6 +10,27 @@ namespace MS.Infrastructure
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddTransient<IValidator<Medicine>, MedicineValidator>();
+            services.AddTransient<IValidator<Types>, TypesValidator>();
+            services.AddTransient<IValidator<Pharmacy>, PharmacyValidator>();
+            services.AddTransient<IValidator<MedicineType>, MedicineTypeValidator>();
+            services.AddTransient<IValidator<PharmacyMedicine>, PharmacyMedicineValidator>();
+            services.AddTransient<IValidator<Reservation>, ReservationValidator>();
+            services.AddTransient<IValidator<TestLab>, TestLabValidator>();
+            services.AddTransient<IValidator<Department>, DepartmentValidator>();
+            services.AddTransient<IValidator<Shift>, ShiftValidator>();
+            services.AddTransient<IValidator<Clinic>, ClinicValidator>();
+            services.AddTransient<IValidator<Labs>, LabsValidator>();
+            services.AddTransient<IValidator<Tests>, TestsValidator>();
+            services.AddTransient<IValidator<Hospital>, HospitalValidator>();
+            services.AddTransient<IValidator<User>, UserValidator>();
+            services.AddTransient<IValidator<Report>, ReportValidator>();
+            services.AddTransient<IValidator<Documents>, DocumentsValidator>();
+            services.AddTransient<IValidator<ReportMedicine>, ReportMedicineValidator>();
+            services.AddTransient<IValidator<Equipment>, EquipmentValidator>();
+            services.AddTransient<IValidator<AttachmentDocuments>, AttachmentDocumentsValidator>();
+            services.AddTransient<IValidator<PlaceEquipment>, PlaceEquipmentValidator>();
+            services.AddTransient<IValidator<EntityAuth>, EntityAuthValidator>();
+            services.AddTransient<IValidator<ClinicPrice>, ClinicPriceValidator>();
         }
     }
 }
