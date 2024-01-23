15fb3c4ff22dc8ec381cf5253c834d4482af11fa
Zeyad Abdelnaby <zeyadabdelnaby15@gmail.com>
2024-01-23 12:40:15 +0200

    validator template

---
+++ BackEnd/MS.Infrastructure/Validation/HospitalValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using MS.Data.Entities;
+
+namespace MS.Infrastructure.Validation
+{
+    public class HospitalValidator : AbstractValidator<Hospital>
+    {
+        public HospitalValidator()
+        {
+            RuleFor(x => x.ID).NotEmpty();
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Phone).NotEmpty().Length(11); 
+            RuleFor(x => x.Government).NotEmpty();
+            RuleFor(x => x.City).NotEmpty();
+            RuleFor(x => x.Country).NotEmpty();
+            RuleFor(x => x.Type).NotEmpty();
+        }
+    }
+}
