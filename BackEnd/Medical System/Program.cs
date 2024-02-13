9fb286d76dcd55bfbd214e3bd9490dfd21a71434
Zeyad Abdelnaby <zeyadabdelnaby15@gmail.com>
2024-02-13 20:17:02 +0200

    بص كده يمعلم دن بنسبة 90%

--- BackEnd/Medical System/Program.cs
+++ BackEnd/Medical System/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container
 builder.Services.AddControllers();
 builder.Services.Appplicatiion_CS(builder.Configuration);
-builder.Services.Infrastructure_CS(builder.Configuration);
+builder.Services.Infrastructure_CS();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
