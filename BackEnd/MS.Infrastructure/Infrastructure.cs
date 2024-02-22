7646a30e78cbdb447c4395c859278749b838a27e
Zeyad Abdelnaby <zeyadabdelnaby15@gmail.com>
2024-02-22 18:38:00 +0200

    cleaning errors

--- BackEnd/MS.Infrastructure/Infrastructure.cs
+++ BackEnd/MS.Infrastructure/Infrastructure.cs
@@ -7,8 +7,6 @@ using MS.Data.Entities;
 using MS.Infrastructure.Contexts;
 using MS.Infrastructure.Repositories.Generics;
 using MS.Infrastructure.Repositories.UnitOfWork;
-using MS.Infrastructure.Validation;
-
 namespace MS.Infrastructure
 {
     public static class Infrastructure
@@ -16,29 +14,6 @@ namespace MS.Infrastructure
 
         public static void Infrastructure_CS(this IServiceCollection services)
         {
-            #region Validation Resolving
-            services.AddTransient<IValidator<Medicine>, MedicineValidator>();
-            services.AddTransient<IValidator<Types>, TypesValidator>();
-            services.AddTransient<IValidator<Pharmacy>, PharmacyValidator>();
-            services.AddTransient<IValidator<MedicineType>, MedicineTypeValidator>();
-            services.AddTransient<IValidator<PharmacyMedicine>, PharmacyMedicineValidator>();
-            services.AddTransient<IValidator<Reservation>, ReservationValidator>();
-            services.AddTransient<IValidator<TestLab>, TestLabValidator>();
-            services.AddTransient<IValidator<Department>, DepartmentValidator>();
-            services.AddTransient<IValidator<Shift>, ShiftValidator>();
-            services.AddTransient<IValidator<Clinic>, ClinicValidator>();
-            services.AddTransient<IValidator<Lab>, LabValidator>();
-            services.AddTransient<IValidator<Test>, TestsValidator>();
-            services.AddTransient<IValidator<Hospital>, HospitalValidator>();
-            services.AddTransient<IValidator<ApplicationUser>, UserValidator>();
-            services.AddTransient<IValidator<Report>, ReportValidator>();
-            services.AddTransient<IValidator<Document>, DocumentValidator>();
-            services.AddTransient<IValidator<ReportMedicine>, ReportMedicineValidator>();
-            services.AddTransient<IValidator<Equipment>, EquipmentValidator>();
-            services.AddTransient<IValidator<PlaceEquipment>, PlaceEquipmentValidator>();
-            services.AddTransient<IValidator<EntityAuth>, EntityAuthValidator>();
-            services.AddTransient<IValidator<ClinicPrice>, ClinicPriceValidator>();
-            #endregion
             services.AddTransient<IUnitOfWork,UnitOfWork>();
         }
     }
