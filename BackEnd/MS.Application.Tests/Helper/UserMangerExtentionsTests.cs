ad97e44faef0aad40a2f9bfc039a727200b46c6b
Alaa Mobarak <3laa.m0o0barak@gmail.com>
2024-04-03 20:29:48 +0200

    application layer semi done

---
+++ BackEnd/MS.Application.Tests/Helper/UserMangerExtentionsTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using MS.Application.Helpers.UserManagerExtensions;
+using System.Security.Claims;
+using Xunit;
+
+public class UserManagerExtensionsTests
+{
+    [Fact]
+    public void GetCurrentUserId_ReturnsUserId()
+    {
+        // Arrange
+        var userManagerMock = new Mock<UserManager<IdentityUser>>(Mock.Of<IUserStore<IdentityUser>>(), null, null, null, null, null, null, null, null);
+        var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+        var expectedUserId = "test-user-id";
+
+        var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+        {
+            new Claim("uid", expectedUserId),
+        }));
+
+        httpContextAccessorMock.Setup(x => x.HttpContext.User)
+            .Returns(claimsPrincipal);
+
+        // Act
+        var actualUserId = userManagerMock.Object.GetCurrentUserId(httpContextAccessorMock.Object);
+
+        // Assert
+        Assert.Equal(expectedUserId, actualUserId);
+    }
+
+    [Fact]
+    public void GetCurrentUserId_ThrowsUnauthorizedAccessException_WhenUserIdIsNull()
+    {
+        // Arrange
+        var userManagerMock = new Mock<UserManager<IdentityUser>>(Mock.Of<IUserStore<IdentityUser>>(), null, null, null, null, null, null, null, null);
+        var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+
+        var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new Claim[] { }));
+
+        httpContextAccessorMock.Setup(x => x.HttpContext.User)
+            .Returns(claimsPrincipal);
+
+        // Act & Assert
+        Assert.Throws<UnauthorizedAccessException>(() => userManagerMock.Object.GetCurrentUserId(httpContextAccessorMock.Object));
+    }
+}
